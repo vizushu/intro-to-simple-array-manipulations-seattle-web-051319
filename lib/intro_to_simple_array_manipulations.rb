a0e15db9141cb377576d1546ac3df65ab19997f5
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-04 17:33:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,3 +35,17 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete(integer)
+end
+  
+  
+  
+  
