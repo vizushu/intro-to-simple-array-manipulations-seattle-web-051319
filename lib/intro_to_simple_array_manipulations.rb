5c7b15058fa84e2f735b214488fd508ca8ba5f20
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-04 17:19:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,22 @@
 def using_push(array, string)
   array.push(string)
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop[-1, -2]
+end
+
+def shift_with_args(array)
+  array.shift[0, 1]
+end
+
+def using_concat(array1, array2)
+  
